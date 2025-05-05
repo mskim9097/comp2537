d92e9191c3f2dd66601692c5bfb5275615a36fc0
mskim9097 <mskim9097@gmail.com>
2025-05-05 04:51:43 -0700

    edit lab2

--- Lab2/app.js
+++ Lab2/app.js
@@ -7,63 +7,27 @@ const fs = require('fs');
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 // Index
 app.get('/', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/index.html', 'utf8');
-    res.send(html);
+    res.render("index");
 });
 
 // Color Indexes
-app.get('/blue', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/blue.html', 'utf8');
-    res.send(html);
-});
-app.get('/red', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/red.html', 'utf8');
-    res.send(html);
-});
-app.get('/green', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/green.html', 'utf8');
-    res.send(html);
+app.get(/^\/(red|blue|green)$/, (req, res) => {
+    const color = req.params[0];
+    res.render('color', { color });
 });
 
 // Color Pages with Sizes
-app.get('/blue/20', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/blue20.html', 'utf8');
-    res.send(html);
-});
-app.get('/blue/30', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/blue30.html', 'utf8');
-    res.send(html);
-});
-app.get('/blue/40', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/blue40.html', 'utf8');
-    res.send(html);
-});
-app.get('/red/20', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/red20.html', 'utf8');
-    res.send(html);
-});
-app.get('/red/30', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/red30.html', 'utf8');
-    res.send(html);
-});
-app.get('/red/40', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/red40.html', 'utf8');
-    res.send(html);
-});
-app.get('/green/20', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/green20.html', 'utf8');
-    res.send(html);
-});
-app.get('/green/30', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/green30.html', 'utf8');
-    res.send(html);
-});
-app.get('/green/40', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/green40.html', 'utf8');
-    res.send(html);
+app.get(/^\/(red|blue|green)\/(20|30|40)$/, (req, res) => {
+    const match = req.url.match(/^\/(red|blue|green)\/(20|30|40)$/);
+
+    const color = match[1];
+    const size = match[2];
+
+    res.render('color-size', { color, size });
 });
 
 // Start the server
